5f6f42f5f75cf9eb5f59d04108b6a3c8f99b3d8e
Lukáš Novotný <lukas@dramiel.com>
2012-09-08 22:24:26 +0200

    added html attribute dictionary

---
+++ src/Clutch.Web/HtmlAttributeDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Routing;
+
+namespace Clutch.Web
+{
+	/// <summary>
+	/// Dictionary with functionality to replace underscores to hypens in keys
+	/// </summary>
+	public class HtmlAttributeDictionary : RouteValueDictionary
+	{
+		public HtmlAttributeDictionary()
+			: base()
+		{ }
+		public HtmlAttributeDictionary(object values)
+			: base(values)
+		{
+			ReplaceUnderscores();
+		}
+		public HtmlAttributeDictionary(IDictionary<string, object> dictionary)
+			: base(dictionary)
+		{
+			ReplaceUnderscores();
+		}
+
+		public void ReplaceUnderscores()
+		{
+			var pairsToReplace = this.Where(p => p.Key.Contains("_")).ToArray();
+
+			foreach (var pair in pairsToReplace)
+			{
+				this.Remove(pair.Key);
+				this.Add(pair.Key.Replace("_", "-"), pair.Value);
+			}
+		}
+	}
+}
